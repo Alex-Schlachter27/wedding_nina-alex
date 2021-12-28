b293123584414c8e48bf903a21959ae7697910be
NIRAS\ALES <ales@niras.dk>
2021-12-28 18:08:47 +0100

    test

---
+++ test/src/app/navigation/header/header.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppService } from 'src/app/app.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  @Output() public sidenavToggle = new EventEmitter();
+  loading?: boolean;
+  
+  constructor(
+    private router: Router,
+    private appSv: AppService
+  ) { }
+
+  ngOnInit(): void {
+    // this.appSv.loading$.subscribe(status => {
+    //   this.loading = status;
+    // })
+  }
+  
+  public onToggleSidenav = () => {
+    this.sidenavToggle.emit();
+  }
+
+  forceNavigate(name: string) {
+    this.router.navigate(['/paragraph'], { fragment: name });
+  }
+}
