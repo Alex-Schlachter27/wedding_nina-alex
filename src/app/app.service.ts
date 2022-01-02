63a21896382b6a8a74700cb02c45c61a792afdd4
Alex-Schlachter27 <62239809+Alex-Schlachter27@users.noreply.github.com>
2022-01-02 11:59:45 +0100

    Delete src directory

--- src/app/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { Location } from '@angular/common';
-import { BehaviorSubject } from 'rxjs';
-
-@Injectable()
-export class AppService {
-  loading$ = new BehaviorSubject<any>(null);
-}
